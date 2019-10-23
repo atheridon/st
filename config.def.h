bc275a47bf598636cf94f7a926999e3f9006b7f3
recktard <recktarded@gmail.com>
2019-10-23 18:10:50 +0200

    no changes

--- config.def.h
+++ config.def.h
@@ -83,7 +83,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-unsigned int alpha = 0xee;
+unsigned int alpha = 0xde;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
