6dbdf4f1d7c87399c65d036d6cba066a003ca1d6
atheridon <atheridon@gmail.com>
2020-12-21 21:04:00 +0100

    changed font size

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "IBMPlexMono:regular:pixelsize=18:antialias=true:autohint=true";
+static char *font = "IBMPlexMono:regular:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 10;
 
 /*
